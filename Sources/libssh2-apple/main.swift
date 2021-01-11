8a2e6c2c86eb9309d9be3875e9300b320b328303
Yury Korolev <yury.korolev@gmail.com>
2021-01-11 09:44:23 +0300

    Build

--- Sources/libssh2-apple/main.swift
+++ Sources/libssh2-apple/main.swift
@@ -102,7 +102,7 @@ for p in Config.platforms {
       "-DBUILD_TESTING=NO",
       "-DENABLE_ZLIB_COMPRESSION=YES",
       "-DENABLE_CRYPT_NONE=YES",
-      "-DENABLE_MAC_NONE=YES",
+      "-DENABLE_MAC_NONE=YES"
       , env: env)
     
     try sh(
